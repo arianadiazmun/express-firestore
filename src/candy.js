d1406ac2e2f7f1d43a7cdd37990240602d0e2313
Ariana Diaz <arianadiaz@Arianas-MacBook-Air.local>
2023-06-21 11:53:22 -0400

    express-candy-firestore

---
+++ src/candy.js
@@ -0,0 +1,25 @@
+
+import { db } from "./connectDb.js"
+
+
+export async function getCandy(req, res) {
+   const candy = await db.collection('candy').get()
+          .catch (err=> {
+            res.status(500).send({success: false, message: err})
+             return
+          }) 
+ //Now let's clean up candy
+ const candyArray = candy.docs.map(doc => ({id: doc.id, ...doc.data() }))
+ res.send(candyArray)
+}
+
+export async function addNewCandy(req,res) {
+   const newCandy = req.body 
+  await db.collection('candy').add(newCandy)
+.catch(err =>{
+   res.status(500).send({success: false, message: err})
+   return
+})
+getCandy(req, res) 
+
+}
